d2b0118fc18de7b5d7c52ec3766f66416db5e01b
Lucas Palharini <lucas230991@hotmail.com>
2025-01-10 00:51:46 -0300

    chore: migrate adonis to v6

--- backend/start/env.ts
+++ backend/start/env.ts
@@ -1,24 +1,38 @@
 /*
 |--------------------------------------------------------------------------
-| Validating Environment Variables
+| Environment variables service
 |--------------------------------------------------------------------------
 |
-| In this file we define the rules for validating environment variables.
-| By performing validation we ensure that your application is running in
-| a stable environment with correct configuration values.
-|
-| This file is read automatically by the framework during the boot lifecycle
-| and hence do not rename or move this file to a different location.
+| The `Env.create` method creates an instance of the Env service. The
+| service validates the environment variables and also cast values
+| to JavaScript data types.
 |
 */
-import { Env } from "@adonisjs/core/env";
 
-export default await Env.create(new URL("../", import.meta.url), {
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+import { Env } from '@adonisjs/core/env'
+
+export default await Env.create(new URL('../', import.meta.url), {
+    NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+    PORT: Env.schema.number(),
+    APP_KEY: Env.schema.string(),
+    HOST: Env.schema.string({ format: 'host' }),
+    LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+
+    /*
+  |----------------------------------------------------------
+  | Variables for configuring database connection
+  |----------------------------------------------------------
+  */
+    DB_HOST: Env.schema.string({ format: 'host' }),
+    DB_PORT: Env.schema.number(),
+    DB_USER: Env.schema.string(),
+    DB_PASSWORD: Env.schema.string.optional(),
+    DB_DATABASE: Env.schema.string(),
+
+    /*
+  |----------------------------------------------------------
+  | Variables for configuring session package
+  |----------------------------------------------------------
+  */
+    SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
 })
-
