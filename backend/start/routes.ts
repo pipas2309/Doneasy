68ee622b5d00b7250c7998cdbbe8f4827bb5c097
Lucas Palharini <lucas230991@hotmail.com>
2025-01-12 03:06:55 -0300

    ✨ feat: add authentication routes (register, login, logout)

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,9 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AuthController from '#controllers/auth_controller'
+import UsersController from '#controllers/users_controller'
+import { middleware } from '#start/kernel'
+import TasksController from "#controllers/tasks_controller";
+
+router.post('/register', AuthController.prototype.register)
+router.post('/login', AuthController.prototype.login)
+router.post('/logout', AuthController.prototype.logout)
 
 router.get('/', async () => {
     return {
-        mim: '2reau',
+        healthCheck: 'Ok',
     }
 })
