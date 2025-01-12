27e870030da5f92b4b623de355e6481ca9c11e16
Lucas Palharini <lucas230991@hotmail.com>
2025-01-12 02:23:11 -0300

    ✨ feat: implement update user validator

---
+++ backend/app/validators/update_user.ts
@@ -0,0 +1,55 @@
+import vine, { SimpleMessagesProvider } from '@vinejs/vine'
+import { uniqueRule } from '#rules/unique'
+import { cpfRule } from '#rules/cpf'
+
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{4,}$/
+
+export const updateUserValidator = vine.compile(
+    vine.object({
+        email: vine
+            .string()
+            .email()
+            .normalizeEmail()
+            .optional()
+            .use(uniqueRule({ table: 'users', column: 'email' })),
+
+        username: vine
+            .string()
+            .minLength(3)
+            .maxLength(24)
+            .optional()
+            .use(uniqueRule({ table: 'users', column: 'username' })),
+
+        fullName: vine.string().maxLength(254).optional(),
+
+        cpf: vine
+            .string()
+            .minLength(11)
+            .maxLength(14)
+            .optional()
+            .use(cpfRule())
+            .use(uniqueRule({ table: 'users', column: 'cpf' })),
+
+        oldPassword: vine.string()
+            .optional()
+            .requiredIfExists('newPassword'),
+
+        newPassword: vine.string()
+            .minLength(8)
+            .maxLength(32)
+            .regex(passwordRegex)
+            .optional()
+            .requiredIfExists('oldPassword'),
+    })
+)
+
+const messages = {
+    'email.email': 'Informe um e-mail válido.',
+    'username.minLength': 'O username deve ter pelo menos 3 caracteres.',
+    'username.maxLength': 'O username deve ter no máximo 24 caracteres.',
+    'newPassword.regex': 'A nova senha precisa de maiúscula, minúscula, número e especial.',
+    'newPassword.minLength': 'A nova senha precisa de pelo menos 8 dígitos.',
+    'newPassword.maxLength': 'A nova senha precisa, no máximo, de 32 dígitos.',
+}
+
+vine.messagesProvider = new SimpleMessagesProvider(messages)
