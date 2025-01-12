62617ee3ff15c5886e048163d0691aaceca5445c
Lucas Palharini <lucas230991@hotmail.com>
2025-01-12 03:10:26 -0300

    feat: add user routes and tasks routes

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -17,7 +17,22 @@ router.post('/register', AuthController.prototype.register)
 router.post('/login', AuthController.prototype.login)
 router.post('/logout', AuthController.prototype.logout)
 
-router.get('/', async () => {
+router
+    .group(() => {
+        router.get('/me', UsersController.prototype.me)
+        router.put('/me', UsersController.prototype.update)
+        router.put('/me/password', UsersController.prototype.changePassword)
+    })
+    .use(middleware.auth())
+
+router
+    .group(() => {
+        router.put('/tasks/reorder', TasksController.prototype.reorder)
+        router.resource('/tasks', TasksController).apiOnly()
+    })
+    .use(middleware.auth())
+
+router.get('/health-check', async () => {
     return {
         healthCheck: 'Ok',
     }
