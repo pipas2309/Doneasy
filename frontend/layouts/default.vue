f870f245d626ed1557a39c5128bdd579922cf283
Lucas Palharini <lucas230991@hotmail.com>
2025-01-13 02:22:03 -0300

    feat: add default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <Headerbar :on-menu-click="toggleSidebar" />
+    <div class="flex h-screen">
+
+        <Sidebar :is-open="isSidebarOpen" :on-close="closeSidebar"/>
+
+        <main class="flex-1 p-4 bg-gray-50 overflow-y-auto">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+const isSidebarOpen = ref(false)
+const { checkAuth } = useAuth()
+
+function toggleSidebar() {
+    isSidebarOpen.value = !isSidebarOpen.value
+}
+
+function closeSidebar() {
+    isSidebarOpen.value = false
+}
+
+onMounted(() => {
+    checkAuth()
+})
+</script>
