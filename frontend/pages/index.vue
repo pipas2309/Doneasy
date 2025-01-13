09e7c17ef296a14c07a071de0b0a62843bcd728c
Lucas Palharini <lucas230991@hotmail.com>
2025-01-13 02:16:55 -0300

    fix: index.vue to redirecting to '/login'

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-const route = useRoute()
+const route = useRouter()
+onMounted(() => {
+    route.replace('/login')}
+)
 </script>
 
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-  </div>
+    <div>Redirecionando...</div>
 </template>
