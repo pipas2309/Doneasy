ed0e0abdc784ec22003b47dbd17e22f5b52ef45b
Lucas Palharini <lucas230991@hotmail.com>
2025-01-17 02:02:37 -0300

    fix: ensure custom validation messages are applied in loginValidator

--- backend/app/validators/login.ts
+++ backend/app/validators/login.ts
@@ -1,5 +1,12 @@
 import vine, { SimpleMessagesProvider } from '@vinejs/vine'
 
+const messages = {
+    'username.required': 'O campo de usuário é obrigatório.',
+    'password.required': 'O campo da senha é obrigatório.',
+}
+
+vine.messagesProvider = new SimpleMessagesProvider(messages)
+
 export const loginValidator = vine.compile(
     vine.object({
         username: vine.string().optional().requiredIfMissing('email'),
@@ -8,10 +15,3 @@ export const loginValidator = vine.compile(
         password: vine.string(),
     })
 )
-
-const messages = {
-    'username.required': 'O campo de usuário é obrigatório.',
-    'password.required': 'O campo da senha é obrigatório.',
-}
-
-vine.messagesProvider = new SimpleMessagesProvider(messages)
